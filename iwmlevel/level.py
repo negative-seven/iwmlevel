e12b02016411565428a5984c74131eaad687bc3c
negative-seven <tehseven1@gmail.com>
2021-03-20 23:04:48 +0100

    Organize Level class

--- iwmlevel/level.py
+++ iwmlevel/level.py
@@ -1,6 +1,6 @@
 import struct
 from xml.etree import ElementTree
-from xml.etree.ElementTree import Element, ElementTree, SubElement
+from xml.etree.ElementTree import Element, SubElement
 
 from .graphics_data import GraphicsData
 
@@ -9,27 +9,28 @@ class Level:
 	SCREEN_WIDTH = 800
 	SCREEN_HEIGHT = 608
 
-	def __init__(self):
-		self.name = ''
-		self.version = 71 # todo
+	def __init__(self, name = '', size = (1, 1), game_version = 76):
+		self.name = name
+		self.version = game_version
 		self.block_0_graphics = GraphicsData()
 		self.block_1_graphics = GraphicsData()
 		self.background_graphics = GraphicsData()
 		self.spike_graphics = GraphicsData()
-		self.size = (1, 1)
+		self.size = size
 		self.scroll_mode_id = 0
 		self.music_id = 0
+		self.objects = []
 
-	def get_pixel_width(self):
+	def get_pixel_width(self) -> int:
 		return Level.SCREEN_WIDTH * self.size[0]
 	
-	def get_pixel_height(self):
+	def get_pixel_height(self) -> int:
 		return Level.SCREEN_HEIGHT * self.size[1]
 
-	def save(self, filepath: str):
-		xml_root = Element('sfm_map')
+	def to_xml(self) -> Element:
+		xml_map = Element('sfm_map')
 
-		xml_head = SubElement(xml_root, 'head')
+		xml_head = SubElement(xml_map, 'head')
 
 		for name, value in [
 			('name', self.name),
@@ -43,7 +44,7 @@ class Level:
 			('colors', None), # handled later
 			('scroll_mode', self.scroll_mode_id),
 			('music', self.music_id),
-			('num_objects', 0), # todo
+			('num_objects', len(self.objects)),
 		]:
 			xml_head_subelement = SubElement(xml_head, name)
 			xml_head_subelement.text = str(value)
@@ -73,7 +74,13 @@ class Level:
 						colors_string_append_float(graphics_data.color.as_hsv_tuple()[hsv_index])
 		xml_head.find('colors').text = colors_string
 
-		xml_objects = SubElement(xml_root, 'objects')
+		xml_objects = SubElement(xml_map, 'objects')
 
-		with open(filepath, 'wb') as file:
-			ElementTree(xml_root).write(file)
+		return xml_map
+
+	def to_xml_string(self) -> str:
+		return ElementTree.tostring(self.to_xml(), encoding='utf-8').decode('utf-8')
+
+	def save(self, filepath: str) -> None:
+		with open(filepath, 'w') as f:
+			f.write(self.to_xml_string())
