51ed633b4b3a412416954eac544ae1203b70912e
negative-seven <tehseven1@gmail.com>
2021-03-21 20:20:32 +0100

    Slotted LevelObjects

--- iwmlevel/levelobject.py
+++ iwmlevel/levelobject.py
@@ -3,18 +3,22 @@ from xml.etree.ElementTree import Element, SubElement
 
 
 class LevelObject:
-	def __init__(self, type_id: int, x = 0, y = 0, parameters = dict()):
+	def __init__(self, type_id: int, x = 0, y = 0, parameters = dict(), slot = None):
 		self.type_id = type_id
 		self.x = x
 		self.y = y
+		self.slot = slot
 		self.events = []
 		self.parameters = parameters
+		self.slotted_objects = []
 
 	def to_xml(self) -> Element:
 		xml_object = Element('object')
 		xml_object.attrib['type'] = str(self.type_id)
 		xml_object.attrib['x'] = str(self.x)
 		xml_object.attrib['y'] = str(self.y)
+		if self.slot is not None:
+			xml_object.attrib['slot'] = str(self.slot)
 
 		for event in self.events:
 			xml_object.append(event.to_xml())
@@ -23,6 +27,11 @@ class LevelObject:
 			xml_parameter = SubElement(xml_object, 'param')
 			xml_parameter.attrib['key'] = key
 			xml_parameter.attrib['val'] = str(value)
+
+		for slotted_object in self.slotted_objects:
+			xml_slotted_object = slotted_object.to_xml()
+			xml_slotted_object.tag = 'obj'
+			xml_object.append(xml_slotted_object)
 
 		return xml_object
 
