cc859e122e15ebd68d7fe6853f9a39a3cb9a3e7e
negative-seven <tehseven1@gmail.com>
2021-03-21 13:11:16 +0100

    LevelObject parameters

--- iwmlevel/levelobject.py
+++ iwmlevel/levelobject.py
@@ -1,18 +1,24 @@
 from xml.etree import ElementTree
-from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import Element, SubElement
 
 
 class LevelObject:
-	def __init__(self, type_id: int, x = 0, y = 0):
+	def __init__(self, type_id: int, x = 0, y = 0, parameters = dict()):
 		self.type_id = type_id
 		self.x = x
 		self.y = y
+		self.parameters = parameters
 
 	def to_xml(self) -> Element:
 		xml_object = Element('object')
 		xml_object.attrib['type'] = str(self.type_id)
 		xml_object.attrib['x'] = str(self.x)
 		xml_object.attrib['y'] = str(self.y)
+
+		for key, value in self.parameters.items():
+			xml_parameter = SubElement(xml_object, 'param')
+			xml_parameter.attrib['key'] = key
+			xml_parameter.attrib['val'] = str(value)
 
 		return xml_object
 
